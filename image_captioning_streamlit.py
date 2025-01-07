94494e4f5237474e2ce826dd2dc38c208d1ceb5d
viswanathsr-04 <viswanath04082k04@gmail.com>
2025-01-07 21:17:46 +0530

    Second commit with added @st.cache_resource to optimize

--- image_captioning_streamlit.py
+++ image_captioning_streamlit.py
@@ -3,18 +3,22 @@ import torch
 from PIL import Image
 import streamlit as st
 from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
-from groq import Groq
 from gtts import gTTS
+from groq import Groq
 import io
-import base64
 
 # Initialize Groq client
 groq_client = Groq(api_key=st.secrets["GROQ_API_KEY"])
 
-# Load the image captioning model, feature extractor, and tokenizer
-model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+# Cache the model, processor, and tokenizer to optimize performance
+@st.cache_resource
+def load_model():
+    model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+    processor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+    tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+    return model, processor, tokenizer
+
+model, processor, tokenizer = load_model()
 
 # Configure the device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -26,7 +30,7 @@ num_beams = 4
 gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
 
 def generate_initial_caption(image):
-    pixel_values = feature_extractor(images=[image], return_tensors="pt").pixel_values
+    pixel_values = processor(images=[image], return_tensors="pt").pixel_values
     pixel_values = pixel_values.to(device)
     output_ids = model.generate(pixel_values, **gen_kwargs)
     caption = tokenizer.decode(output_ids[0], skip_special_tokens=True).strip()
@@ -59,7 +63,7 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image", width=400)
     st.write("")
-    with st.spinner("Loading..."):
+    with st.spinner("Generating caption..."):
         initial_caption = generate_initial_caption(image)
         print(initial_caption)
         refined_caption = refine_caption_with_groq(initial_caption)
